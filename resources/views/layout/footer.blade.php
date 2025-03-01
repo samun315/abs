ad69d751433f68566cd0b0df55d3f0fd47811493
Samun Chowdhury <samun315@gmail.com>
2025-03-01 13:06:55 +0600

    header and footer modified

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -3,13 +3,12 @@
     <div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
         <!--begin::Copyright-->
         <div class="text-dark order-2 order-md-1">
-            <span class="text-muted fw-bold me-1">2024©</span>
-            <a href="https://solutionspin.com" target="_blank" class="text-gray-800 text-hover-primary">Technology Partner
-                Solutionspin Limited.</a>
+            <span class="text-muted fw-bold me-1">2025©</span>
+            <a href="https://solutionspin.com" target="_blank" class="text-gray-800 text-hover-primary">OurAbs</a>
         </div>
         <!--end::Copyright-->
         <!--begin::Menu-->
-        <ul class="menu menu-gray-600 menu-hover-primary fw-bold order-1">
+        {{-- <ul class="menu menu-gray-600 menu-hover-primary fw-bold order-1">
             <li class="menu-item">
                 <a href="javascript:void(0)" target="_blank" class="menu-link px-2">About</a>
             </li>
@@ -19,7 +18,7 @@
             <li class="menu-item">
                 <a href="javascript:void(0)" target="_blank" class="menu-link px-2">Purchase</a>
             </li>
-        </ul>
+        </ul> --}}
         <!--end::Menu-->
     </div>
     <!--end::Container-->
